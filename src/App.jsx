f0b2744367f9055eb9950b8291cc235fa603fa10
Andy Lau <landywl4@gmail.com>
2022-01-03 14:28:55 -0800

    Modified the fetch simulation

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@
 
 import { css, jsx } from "@emotion/react";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from "react";
 import Loading from "./components/Loading";
 
 const unclickedCSS = css`
   color: black;
   background: gray;
   border-radius: 2em;
-  padding: .2em;
+  padding: 0.2em;
   &:hover {
     color: red;
     cursor: pointer;
@@ -21,7 +21,7 @@ const clickedCSS = css`
   color: white;
   background: red;
   border-radius: 2em;
-  padding: .2em;
+  padding: 0.2em;
   &:hover {
     color: white;
     background: gray;
@@ -29,29 +29,38 @@ const clickedCSS = css`
   }
 `;
 
+const gifCSS = css`
+`
+
 function App() {
   const [clicked, setClicked] = useState(false);
   const [loadingStatus, setLoadingStatus] = useState(false);
 
   const handleClick = async () => {
-    // const data = (clicked) ? "increment" : "decrement";
-    // const fetchOptions = {
-    //   method: 'POST',
-    //   body: JSON.stringify(data)
-    // }
+    // setLoadingStatus and setTimeout in handleClick to simulate post wait time. Should be moved to callBackend in real use cases
     setLoadingStatus(true);
-    setTimeout(() => {
-      setLoadingStatus(false);
-    }, 1000);
-    // fetch("https://localhost:3000/likedAPI", fetchOptions)
+    setTimeout(callBackend, 1000);
     setClicked(!clicked);
-  }
-  
+  };
+
+  const callBackend = useCallback(() => {
+    // Using giphy search API in place of a increment/decrement likes count API
+    // const data = clicked ? { action: "increment" } : { action: "decrement" };
+    fetch("http://api.giphy.com/v1/gifs/search?api_key=Jy2Ju3zlY96ubjreQjn4M6FbXxdh84uU&q=bananas")
+      .then(res => res.json())
+      .then(res => {
+        setLoadingStatus(false);
+      });
+  }, []);
+
   return (
     <div>
-      {(loadingStatus) ? <Loading /> : (
-        (clicked) ? <FavoriteIcon css={clickedCSS} onClick={handleClick}/> : 
-        <FavoriteIcon css={unclickedCSS} onClick={handleClick}/>
+      {loadingStatus ? (
+        <Loading />
+      ) : clicked ? (
+        <FavoriteIcon css={clickedCSS} onClick={handleClick} />
+      ) : (
+        <FavoriteIcon css={unclickedCSS} onClick={handleClick} />
       )}
     </div>
   );
